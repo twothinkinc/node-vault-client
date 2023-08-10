466776ae14373659d7d8a712c62b99b139abee65
John Adams <jna@retina.net>
2023-08-09 20:11:52 -0700

    move to axios

--- src/VaultApiClient.js
+++ src/VaultApiClient.js
@@ -20,36 +20,59 @@ class VaultApiClient {
         this._logger = logger;
     }
 
-    makeRequest(method, path, data, headers) {
+    async makeRequest(method, path, data, headers) {
         data = data === undefined ? null : data;
         headers = headers === undefined ? {} : headers;
-
+          
         const requestOptions = {
             method: method,
-            body: data === null ? undefined : data,
-            uri: urljoin(this.__config.url, this.__config.apiVersion, path),
-            followRedirects: true,
-            followAllRedirects: true,
-            headers,
-            json: true,
+            data: data === null ? undefined : data,
+            uri: urljoin(this.__config.apiVersion, path),
+            headers: {
+                'Content-Type': 'application/json',
+                ...headers
+            },
             ...this.__config.requestOptions
         };
 
+        const axiosInstance = axios.create({baseURL: this.__config.url});
+
+        // redirect handling
+        axiosInstance.interceptors.response.use(
+            response => response,
+            error => {
+                if (error.response && [301, 302].includes(error.response.status)) {
+                    const redirectUrl = error.response.headers.location;
+                    return axiosInstance.request({
+                        method: requestOptions.method,
+                        url: redirectUrl,
+                        data: requestOptions.data,
+                        headers: requestOptions.headers
+                    });
+                }
+              return Promise.reject(error);
+            }
+        );
+        
         this._logger.debug(
             'making request: %s %s',
             requestOptions.method,
             requestOptions.uri
         );
 
-        return axios(requestOptions)
-            .then((response) => {
-                this._logger.debug('%s %s response body:\n%s',
-                    requestOptions.method,
-                    requestOptions.uri,
-                    JSON.stringify(response, null, ' ')
-                );
-                return response;
-            });
+        return axiosInstance.request({
+            method: requestOptions.method,
+            url: requestOptions.uri,
+            data: requestOptions.data,
+            headers: requestOptions.headers
+        }).then((response) => {
+            this._logger.debug('%s %s response body:\n%s',
+                requestOptions.method,
+                requestOptions.uri,
+                JSON.stringify(response.data, null, ' ')
+            );
+            return response.data;
+        });
     }
 }
 
